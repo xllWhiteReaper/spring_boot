360aef20f33720e2eed7120f711a82db1f942935
A-Cobra <conradbravina@gmail.com>
2023-05-04 21:54:36 -0500

    Created the basic structure for my first spring boot project

---
+++ first_spring_project/src/main/java/com/first_spring_project/first_spring_project/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.first_spring_project.first_spring_project;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FirstSpringProjectApplication.class);
+	}
+
+}
---
+++ first_spring_project/src/test/java/com/first_spring_project/first_spring_project/FirstSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.first_spring_project.first_spring_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

f077d88ba222ef498abe735c5b2e69e6aba86fea
A-Cobra <conradbravina@gmail.com>
2023-05-06 11:42:27 -0500

    Added some basic html and css files to make the project a little bit more aesthetically pleasing

--- first_spring_project/src/main/java/com/first_spring_project/first_spring_project/controllers/UserController.java
+++ first_spring_project/src/main/java/com/first_spring_project/first_spring_project/controllers/UserController.java
@@ -3,11 +3,17 @@ package com.first_spring_project.first_spring_project.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController()
 public class UserController {
 
     @RequestMapping(value = "test")
     public String test() {
-        return "This is a test";
+        return "<h2>This is a test</h2>";
+    }
+
+    @RequestMapping(value = "fruits")
+    public String[] getFruits() {
+        final String[] fruits = { "Apple", "Banana", "Tangerine" };
+        return fruits;
     }
 }

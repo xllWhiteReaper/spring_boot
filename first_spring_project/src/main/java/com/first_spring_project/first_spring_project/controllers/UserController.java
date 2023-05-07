8f457ecec819c2b07b6a9e5b2b65cd92c01ff9b9
A-Cobra <conradbravina@gmail.com>
2023-05-06 22:22:06 -0500

    Added a model and created an endpoint

--- first_spring_project/src/main/java/com/first_spring_project/first_spring_project/controllers/UserController.java
+++ first_spring_project/src/main/java/com/first_spring_project/first_spring_project/controllers/UserController.java
@@ -3,12 +3,35 @@ package com.first_spring_project.first_spring_project.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.first_spring_project.first_spring_project.models.User;
+
 @RestController()
 public class UserController {
 
     @RequestMapping(value = "test")
     public String test() {
         return "<h2>This is a test</h2>";
+    }
+
+    @RequestMapping(value = "users")
+    public User[] getUsers() {
+        User[] users = {
+                new User(
+                        "cobra",
+                        "github",
+                        "fake1@gmail.com",
+                        "123456789",
+                        "definitelyNotMyDefaultPass2022"
+                ),
+                new User(
+                        "xllWhiteReaper",
+                        "github",
+                        "fake2@gmail.com",
+                        "987654321",
+                        "definitelyNotMyDefaultPass"
+                )
+        };
+        return users;
     }
 
     @RequestMapping(value = "fruits")

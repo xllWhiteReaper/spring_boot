16e75248ec52b9948ebcda759eb626a9fd317f71
A-Cobra <conradbravina@gmail.com>
2023-05-08 07:51:18 -0500

    Making an API call and dynamically filling the table

--- first_spring_project/src/main/resources/static/js/users.js
+++ first_spring_project/src/main/resources/static/js/users.js
@@ -3,17 +3,34 @@ const headers = {
   Accept: "application/json",
   "Content-Type": "application/json",
 };
+const TABLE_REFERENCE = document.querySelector("#users-table");
 
 // Call the dataTables jQuery plugin
-$(document).ready(function () {
-  loadUsers();
-  $("#users").DataTable();
+$(document).ready(async function () {
+  const users = await getUsers();
+  fillTable(users);
+  $("#users-table").DataTable();
 });
 
-async function loadUsers() {
-  const request = await fetch(`${API_BASE_URL}/users`, {
+async function getUsers() {
+  return fetch(`${API_BASE_URL}/users`, {
     method: "GET",
     headers,
+  }).then((response) => response.json());
+}
+
+function fillTable(users) {
+  const USER_ROW_TEMPLATE = document.querySelector("#user-information").content;
+  const documentFragment = document.createDocumentFragment();
+  users.forEach((user) => {
+    const rowCopy = document.importNode(USER_ROW_TEMPLATE, true);
+    rowCopy.querySelector(".user_id").innerText = user.id;
+    rowCopy.querySelector(
+      ".user_name"
+    ).innerText = `${user.name} ${user.lastName}`;
+    rowCopy.querySelector(".user_email").innerText = user.email;
+    rowCopy.querySelector(".user_phone").innerText = user.phone;
+    documentFragment.appendChild(rowCopy);
   });
-  users = await request.json();
+  TABLE_REFERENCE.appendChild(documentFragment);
 }

80ae67edcb0e73ac55e37c8a800619e63c109835
A-Cobra <conradbravina@gmail.com>
2023-06-28 07:54:37 -0500

    Created the basic files for the srping boot app

---
+++ rest_api_test/src/main/java/com/xllwhitereaper/rest_api_test/RestApiTestApplication.java
@@ -0,0 +1,13 @@
+package com.xllwhitereaper.rest_api_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiTestApplication.class, args);
+	}
+
+}

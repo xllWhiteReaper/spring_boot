79e2d6787aff7f68553c20d51a418eb278542d7d
A-Cobra <conradbravina@gmail.com>
2023-07-06 08:34:55 -0500

    Finished some methods or the JwtService

--- spring_security/src/main/java/com/xllWhiteReaper/spring_security/config/JwtService.java
+++ spring_security/src/main/java/com/xllWhiteReaper/spring_security/config/JwtService.java
@@ -1,21 +1,52 @@
 package com.xllWhiteReaper.spring_security.config;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
 @Service
 public class JwtService {
     private static final String SECRET_KEY = "edb521a5059c73f0d733e6bd921370760b5d691765b1e0512b525be2dfa18f33b963ff2893a53f6f68ccd696ebfb8f090d3d97465182ff1aaf14cd47499bff4a";
+    private static final long SESSION_DURATION = 1000 * 60 * 60 * 24; // 1 day
 
     public String extractUsername(String jwtToken) {
-        return null;
+        return extractClaim(jwtToken, Claims::getSubject);
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        return Jwts.builder()
+                .setClaims(extraClaims).setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + SESSION_DURATION))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256).compact();
+    }
+
+    public boolean isTokenValid(String jwtToken, UserDetails userDetails) {
+        final String username = extractUsername(jwtToken);
+        return username.equals(userDetails.getUsername()) && !isTokenExpired(jwtToken);
+    }
+
+    private boolean isTokenExpired(String jwtToken) {
+        return extractExpirationDate(jwtToken).before(new Date());
+    }
+
+    private Date extractExpirationDate(String jwtToken) {
+        return extractClaim(jwtToken, Claims::getExpiration);
     }
 
     public <T> T extractClaim(String jwtToken, Function<Claims, T> claimsResolver) {

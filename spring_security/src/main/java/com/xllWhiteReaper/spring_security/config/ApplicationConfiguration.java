2af68a6c57181c05ba3a496a824c91d1dead37e7
A-Cobra <conradbravina@gmail.com>
2023-07-07 07:54:43 -0500

    Created the Security config class

--- spring_security/src/main/java/com/xllWhiteReaper/spring_security/config/ApplicationConfiguration.java
+++ spring_security/src/main/java/com/xllWhiteReaper/spring_security/config/ApplicationConfiguration.java
@@ -11,7 +11,7 @@ import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
-public class ApplicationConfig {
+public class ApplicationConfiguration {
     private final UserRepository userRepository;
 
     @Bean
---
+++ spring_security/src/main/java/com/xllWhiteReaper/spring_security/config/SecurityConfiguration.java
@@ -0,0 +1,29 @@
+package com.xllWhiteReaper.spring_security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+    private final JwtAuthenticationFilter authenticationFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable().authorizeHttpRequests().requestMatchers("").permitAll().anyRequest()
+                .authenticated().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return httpSecurity.build();
+    }
+}
